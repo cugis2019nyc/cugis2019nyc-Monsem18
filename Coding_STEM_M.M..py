94c6611a048e9ff63b8db4fb9ae2cd5cc44e5fb1
Monsem18 <54039971+Monsem18@users.noreply.github.com>
2019-08-13 11:46:19 -0700

    Add files via upload

---
+++ Coding_STEM_M.M..py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Hello World") 
+print ("I am learning to code")
+print ("18")
+5/2
+5*2
+5-2
+8/9*3
+5+2
+3+5
+5**2
+
+def add(a,b):
+    add = a+b
+    print (add)
+    
+add (2,3)
+
+add (10985.345,9802.472)
+
+add (9,18)
+
+add (27,4)
+
+def add(a,b,c):
+    add = a+b+c
+    print (add)
+    
+add (9,18,4)
+
+def subtract(a,b):
+    subtract = a-b
+    print ("the difference of a-b is",subtract)
+    
+subtract (10,6)
+
+subtract (100,31) 
+
+#Jaden's code
+def aqua (a,b,c):
+    aqua = a*b*c
+    print ("the product of",a,b,"and",c,"is",aqua)
+    
+aqua (5,5,5)
+
+#Lydmarie's code
+def area (b,h):
+    area = 1/2*b*h
+    print (area)
+    
+area(1,2)
+
+#Jaelin's code
+def aqua (a,b,c):
+    aqua = 1/2*b*c
+    print ("the area of a triangle with base", b, "and height", c, "is", aqua)
+    
+aqua (1/2,6,9)
+
+#Deniesha's code 
+def star (b,h):
+    star = b*h
+    print ("the area of a triangle with base",b,"and height",h,"is",star)
+    
+star (6,6)
+
+print ("milk chocolate, white chocolate, dark chocolate")
+
+print ("chocolate 1 = milk chocolate")
+print ("chocolate 2 = white chocolate")
+print ("chocolate 3 = dark chocolate")
+
+#choccolate box
+chocolate1 = "milk"
+chocolate2 = "dark"
+chocolate3 = "white"
+
+chocolate1
+print(chocolate1)
+print("chocolate1")
